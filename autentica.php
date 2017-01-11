db1916a0054e7481343f059e18cef92a56e18017
guiferrera <d-igo-guil@hotmail.com>
2017-01-11 09:27:42 -0200

    Add files via upload
    First commit

---
+++ autentica.php
@@ -0,0 +1,35 @@
+﻿<!DOCTYPE html>
+
+<html>
+
+
+<head>
+  
+<meta charset="utf-8">
+  
+<link href="css/style-page.css" rel="stylesheet" />
+  
+<link href="css/bootstrap.css" rel="stylesheet" />    
+  
+<title>Negociação de Mercadorias</title>
+</head>
+
+
+<body>
+
+<?php
+	  
+$login = $_POST['login'];
+	  
+$senha = $_POST['senha'];
+
+
+print $login;
+print $senha;
+
+
+?>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <link href="css/style-page.css" rel="stylesheet" />
+  <link href="css/bootstrap.css" rel="stylesheet" />    
+  <title>Negociação de Mercadorias</title>
+</head>
+
+<body>
+	<header class="container">
+		<h1>Trading of Goods</h1>
+	</header>
+	
+	<section class="container">
+		<div class="painel">
+		
+		<form action="autentica.php" method="POST">
+        	<h4>Login </h4>
+        	<input type="text" name="login" id="login" size="50"><br />
+    
+    		<h4>Senha </h4>
+    		<input type="password" name="senha" id="senha" size="50"><br />
+    
+    		<input type="submit" value="Logar" name="logar" id="logar">
+    		
+		</form>
+		<h4><a href="#">Cadastre-se</a></h4>		
+
+		</div>
+	</section>	
+
+	<footer>
+		<h5>Sistema de Negociação de Mercadorias - By Guilherme Ferreira</h5>
+	</footer>
+</body>
+</html>
